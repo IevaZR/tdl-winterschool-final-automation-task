0ddd36c1fcef7dce3a96f5d7e2ea17170a7402da
Ieva Zvanitaja Rubine <ieva.zvanitaja.rubine@gmail.com>
2024-02-27 01:11:14 +0200

    search scenario added

--- features/search.feature
+++ features/search.feature
@@ -2,12 +2,12 @@ Feature: Search
 # TODO add search which returns also hidden descriptions -> possible to search by color
 # TODO search returns incorrect results also when conducting manual testing
 
-  @search
-  Scenario: Search box returns correct results
+  @searchByProductName
+  Scenario: Search returns correct results - product names
     Given I am on the home page
     When I enter <searchQuery> in the search field
     And I click on search icon
-    Then I get only results containing search query
+    Then I get only results with product names containing search query
 
     Examples: 
       | searchQuery     |
@@ -17,3 +17,20 @@ Feature: Search
       | "dress"         |
       | "printed dress" |
       | "white"         |
+
+  @searchByProductNameAndColor
+  Scenario: Search returns correct results - product names and colors
+    Given I am on the home page
+    When I enter <searchQuery> in the search field
+    And I click on search icon
+    Then I get only results with product names and product colors containing search query
+
+    Examples: 
+      | searchQuery     |
+      | "Blouse"        |
+      | "blouse"        |
+      | "shirt"         |
+      | "dress"         |
+      | "printed dress" |
+      | "white"         |
+      | "purple"        |
--- features/step-definitions/search.js
+++ features/step-definitions/search.js
@@ -11,7 +11,7 @@ When("I click on search icon", async function () {
     await homePage.searchIcon.click()
 })
 
-Then("I get only results containing search query", async function () {
+Then("I get only results with product names containing search query", async function () {
     const receivedProducts = await searchResultsPage.searchResultProducts
     const searchedWords = this.searchQuery.toLowerCase()
     for (const item of receivedProducts) {
@@ -19,6 +19,47 @@ Then("I get only results containing search query", async function () {
         productName = productName.toLowerCase()
         const searchWords = searchedWords.split(' ')
         const allWordsFound = searchWords.every(word => productName.includes(word));
-        expect(allWordsFound).toBe(true);
+        if (!allWordsFound) {
+            await item.moveTo()
+            throw new Error("Product name does not contain the search query.");
+        }
     }
+    return true
+})
+
+Then("I get only results with product names and product colors containing search query", async function() {
+    const products = await searchResultsPage.searchedProducts
+    const searchedWords = this.searchQuery.toLowerCase().split(' ')
+
+    for(const item of products) {
+        let productName = await item.$('h5 a')
+        let productNameText = await productName.getText()
+        productNameText = productNameText.toLowerCase()
+
+        const productColors = await item.$$('.color-list-container a')
+        const allWordsFound = searchedWords.every(word => productNameText.includes(word));
+        if(!allWordsFound) {
+            for(const item of productColors) {
+                const href = await item.getAttribute('href');
+                const colorFound = searchedWords.every(word => href.includes(word));
+                if (!colorFound) {
+                    await item.moveTo()
+                    throw new Error(`Product does not match search query "${this.searchQuery}"`);
+                }
+            }
+        }
+    }
+    // const receivedProductColors = await searchResultsPage.searchResultColorLists
+    // const searchedWords = this.searchQuery.toLowerCase()
+    // for (const item of receivedProducts) {
+    //     let productName = await item.getText()
+    //     productName = productName.toLowerCase()
+    //     const searchWords = searchedWords.split(' ')
+    //     const allWordsFound = searchWords.every(word => productName.includes(word));
+    //     if (!allWordsFound) {
+    //         await item.moveTo()
+    //         throw new Error("Product name does not contain the search query.");
+    //     }
+    // }
+    // return true
 })
